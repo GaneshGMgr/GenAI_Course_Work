bcc5296383479e68547fd79d063df759591f2ec1
ganesh <ganeshwhive980@gmail.com>
2025-08-22 00:07:13 +0545

    feat: complete LangSmith tutorial updates including 4_agent script

--- LangGraph/LangSmith_Tutorial/4_agent.py
+++ LangGraph/LangSmith_Tutorial/4_agent.py
@@ -8,6 +8,7 @@ from langchain import hub
 import os
 from dotenv import load_dotenv
 
+os.environ['LANGCHAIN_PROJECT'] = '4_agent.py.py'
 load_dotenv()
 search_tool = DuckDuckGoSearchRun()
 API_KEY = os.getenv("WEATHERSTACK_API_KEY")
@@ -27,4 +28,26 @@ llm = ChatOllama(model="llama3.2", temperature=0)
 # Step 2: Pull the ReAct prompt from Langchain Hub
 prompt = hub.pull("hwchase17/react") # pulls the standard ReAct agent prompt
 
-# Step 3: Create the ReAct
+# Step 3: Create the ReAct agent manually with the pulled prompt
+agent = create_react_agent(
+    llm = llm,
+    tools = [search_tool, get_weather_data],
+    prompt = prompt
+)
+
+# Step 4: Wrap it with AgentExecutor
+agent_executor = AgentExecutor(
+    agent = agent,
+    tools = [search_tool, get_weather_data],
+    verbos = True,
+    max_iterations = 5
+)
+
+# What is the release date of Agenvers End Game?
+# What is the current temp of Kathmandu?
+# Identify the birthplace city of Pasang Lhamu Sherpa (search) and give its currenct temperature.
+
+# Step 5: Invoke
+response = agent_executor.invoke({"input": "What is the current temp of Kathmandu"})
+print(response)
+print(response['output'])
